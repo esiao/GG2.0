8ddd35f31eb2c7a760740d0945c6e9013630d5e8
Aurelien <aurelien.esiao@gmail.com>
2012-08-27 22:50:13 +0200

    Ce qui existe déjà

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+$(function(){
+$('#pun-navlinks ul li:first-child').remove();
+$('#pun-navlinks').html($('#pun-navlinks').html().replace(/&nbsp;/g,''));
+$('#pun-navlinks ul li:first-child a').html($('#pun-navlinks ul li:first-child a').html().replace(/Portail/g,''));
+$('#pun-navlinks ul li:nth-child(2) a').html($('#pun-navlinks ul li:nth-child(2) a').html().replace(/Accueil/g,'Forum'));
+});
+
+$(function(){
+var url = window.location;
+$('#pun-navlinks a[href="'+ url +'"]').addClass('current');
+$('#pun-navlinks a').filter(function() {
+    return this.href == url;
+}).addClass('current')
+});
+
+$(function(){
+$("a:contains('enregistrer')").addClass('flash');
+$("a:contains('Déconnexion')").addClass('flash');
+});
