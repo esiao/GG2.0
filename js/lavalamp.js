e6b2a79060cb221bf965f3d1f0f2bdd0c7829f95
CronosS <xevious24@hotmail.fr>
2012-08-31 18:47:37 +0300

    lavalamp fix

--- js/lavalamp.js
+++ js/lavalamp.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-	var dleft=$('#pun-navlinks3 li.active').offset().left- $('#pun-navlinks3').offset().left;
-	var dwidth=$('#pun-navlinks3 li.active').width()+"px";
+	var dleft = $('#pun-navlinks3 li.active').offset().left - $('#pun-navlinks3').offset().left;
+	var dwidth=$('#pun-navlinks3 li.active').outerWidth()+"px";
 	$('.floatr').css({"left":dleft+"px","width":dwidth});
-	$('li').hover(function(){
-		var left=$(this).offset().left-($(this).parents('#pun-navlinks3').offset().left+ 15);
-		var width=$(this).width()+"px";
-		var sictranslate="translate("+left+"px, 0px)";$(this).parent('ul').next('div.floatr').css({"width":width,"-webkit-transform":sictranslate,"-moz-transform":sictranslate})
-		;},function(){
-			var left=$(this).siblings('li.active').offset().left-($(this).parents('#pun-navlinks3').offset().left+ 15);
-			var width=$(this).siblings('li.active').width()+"px";
-			var sictranslate="translate("+left+"px, 0px)";
-			$(this).parent('ul').next('div.floatr').css({"width":width,"-webkit-transform":sictranslate,"-moz-transform":sictranslate});}).click(function(){$(this).siblings('li').removeClass('active');$(this).addClass('active');return false;});
+	$('#pun-navlinks3 li').hover(
+		function(){
+			var left= $(this).offset().left - $('#pun-navlinks3').offset().left;
+			var width=$(this).outerWidth()+"px";
+			$(this).parent('ul').next('div.floatr').css({"width":width,"left":left})
+			;},
+		function(){
+			var left=$(this).siblings('li.active').offset().left-($(this).parents('#pun-navlinks3').offset().left);
+			var width=$(this).siblings('li.active').outerWidth()+"px";
+			$(this).parent('ul').next('div.floatr').css({"width":width,"left":left});
+		}
+	).click(function(){
+		$(this).siblings('li').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	});
 });
