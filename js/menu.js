99af40f9ec5eb8f244f6c4d8a44c49ec86547857
Aurelien <aurelien.esiao@gmail.com>
2012-09-13 19:02:56 +0200

    menu final bugs résolu (1/2)

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
-$('#pun-navlinks ul li:first-child').remove();
-$('#pun-navlinks').html($('#pun-navlinks').html().replace(/&nbsp;/g,''));
-$('#pun-navlinks ul li:first-child a').html($('#pun-navlinks ul li:first-child a').html().replace(/Portail/g,''));
-$('#pun-navlinks ul li:nth-child(2) a').html($('#pun-navlinks ul li:nth-child(2) a').html().replace(/Accueil/g,'Forum'));
-});
-
-$(function(){
 var url = window.location;
 $('#pun-navlinks a[href="'+ url +'"]').addClass('current');
 $('#pun-navlinks a').filter(function() {
     return this.href == url;
-}).addClass('current')
+}).parent().addClass('current')
 });
 
 $(function(){
